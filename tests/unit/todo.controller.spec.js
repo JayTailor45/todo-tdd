8e85a39914a25cd40c1dcd29d8a1db3456bc565b
Jay Tailor <tailorj64@gmail.com>
2023-08-10 14:03:36 +0530

    Mock req body

--- tests/unit/todo.controller.spec.js
+++ tests/unit/todo.controller.spec.js
@@ -1,5 +1,9 @@
+const httpMocks = require("node-mocks-http");
+
 const TodoController = require("../../controllers/todo.controller");
 const TodoModel = require("../../models/todo.model");
+
+const newTodo = require('../mock-data/new-todo.json');
 
 TodoModel.create = jest.fn();
 
@@ -8,7 +12,15 @@ describe("TodoController.createTodo", () => {
     expect(typeof TodoController.createTodo).toBe("function");
   });
   it("Should call TodoModel.create", () => {
-    TodoController.createTodo();
-    expect(TodoModel.create).toBeCalled();
+    let req, res, next;
+
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+
+    req.body = newTodo;
+
+    TodoController.createTodo(req, res, next);
+    expect(TodoModel.create).toBeCalledWith(newTodo);
   });
 });
