d256cd6a137124ce5116da24a13dcf1ba6cb8058
Jay Tailor <tailorj64@gmail.com>
2023-08-10 13:47:58 +0530

    Basic tests for todo controller

--- tests/unit/todo.controller.spec.js
+++ tests/unit/todo.controller.spec.js
@@ -1,7 +1,14 @@
 const TodoController = require("../../controllers/todo.controller");
+const TodoModel = require("../../models/todo.model");
+
+TodoModel.create = jest.fn();
 
 describe("TodoController.createTodo", () => {
   it("Should have a createTodo function", () => {
     expect(typeof TodoController.createTodo).toBe("function");
+  });
+  it("Should call TodoModel.create", () => {
+    TodoController.createTodo();
+    expect(TodoModel.create).toBeCalled();
   });
 });
