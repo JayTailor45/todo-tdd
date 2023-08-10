1bcbe8d25fb1be2aa89e4815ccb70654ebceae56
Jay Tailor <tailorj64@gmail.com>
2023-08-10 14:14:39 +0530

    Re-use some code

--- tests/unit/todo.controller.spec.js
+++ tests/unit/todo.controller.spec.js
@@ -3,23 +3,24 @@ const httpMocks = require("node-mocks-http");
 const TodoController = require("../../controllers/todo.controller");
 const TodoModel = require("../../models/todo.model");
 
-const newTodo = require('../mock-data/new-todo.json');
+const newTodo = require("../mock-data/new-todo.json");
 
 TodoModel.create = jest.fn();
+
+let req, res, next;
+
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
 
 describe("TodoController.createTodo", () => {
   it("Should have a createTodo function", () => {
     expect(typeof TodoController.createTodo).toBe("function");
   });
   it("Should call TodoModel.create", () => {
-    let req, res, next;
-
-    req = httpMocks.createRequest();
-    res = httpMocks.createResponse();
-    next = null;
-
     req.body = newTodo;
-
     TodoController.createTodo(req, res, next);
     expect(TodoModel.create).toBeCalledWith(newTodo);
   });
