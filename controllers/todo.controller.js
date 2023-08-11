761cd0dcb09f6d2f42fcac2b9ef362229fa9070a
Jay Tailor <tailorj64@gmail.com>
2023-08-11 15:18:09 +0530

    Test: Unit test for handling error while creating add todo

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,6 +1,10 @@
 const TodoModel = require("../models/todo.model");
 
 exports.createTodo = async (req, res, next) => {
-  const createdTodo = await TodoModel.create(req.body);
-  res.status(201).json(createdTodo);
+  try {
+    const createdTodo = await TodoModel.create(req.body);
+    res.status(201).json(createdTodo);
+  } catch (error) {
+    next(error);
+  }
 };
